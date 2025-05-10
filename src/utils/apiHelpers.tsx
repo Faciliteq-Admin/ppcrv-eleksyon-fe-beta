3037d6662eb4d9c9c5062e904d2678920063f28b
jomar.furiscal <jomar.furiscal@barapido.com>
2025-05-10 16:02:43 +0800

    handle api key

--- src/utils/apiHelpers.tsx
+++ src/utils/apiHelpers.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getUserSession } from "./functions";
 
 const BASE_URL = process.env.REACT_APP_BASEURL;
-const API_KEY = process.env.REACT_APP_APIKEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const getHeaders = () => {
     const headers: any = { 'x-api-key': API_KEY };
